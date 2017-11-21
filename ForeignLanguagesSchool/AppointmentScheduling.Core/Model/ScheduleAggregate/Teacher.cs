ea0fee457ed3e9dd0c3f81cfeb5c638a2582cc96
altair961 <altair961@github.com>
2017-11-21 07:36:21 +0100

    Added Teacher class

---
+++ ForeignLanguagesSchool/AppointmentScheduling.Core/Model/ScheduleAggregate/Teacher.cs
@@ -0,0 +1,17 @@
+﻿using SharedKernel;
+
+namespace AppointmentScheduling.Core.Model.ScheduleAggregate
+{
+    public class Teacher : Entity<int>
+    {
+        public virtual string Name { get; set; }
+        public Teacher(int id)
+            : base(id)
+        {        
+        }
+
+        private Teacher()
+        {
+        }
+    }
+}
