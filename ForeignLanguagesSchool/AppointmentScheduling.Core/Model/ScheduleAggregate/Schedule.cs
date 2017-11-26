a8e28415226207c56add6e3d37492a78d7279f12
altair961 <altair961@github.com>
2017-11-26 13:47:44 +0400

    Added Schedule class

---
+++ ForeignLanguagesSchool/AppointmentScheduling.Core/Model/ScheduleAggregate/Schedule.cs
@@ -0,0 +1,34 @@
+﻿using SharedKernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AppointmentScheduling.Core.Model.ScheduleAggregate
+{
+    public class Schedule : Entity<Guid>
+    {
+        public int SchoolId { get; private set; }
+
+        //not persisted
+        public DateTimeRange DateRange { get; private set; }
+
+        private List<Appointment> appointments;
+        public IEnumerable<Appointment> Appointments
+        {
+            get
+            {
+                return appointments.AsEnumerable(); 
+            }
+            set
+            {
+                appointments = (List<Appointment>)value;
+            }
+        }
+
+        public Schedule(Guid id, DateTimeRange dateRange)
+            : base(id)
+        {
+            // DateRange = 
+        }
+    }
+}
